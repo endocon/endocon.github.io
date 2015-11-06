c4768490e6f73fd830150afd0e61700a86e7dfc2
(cdep)illabout <cdep.illabout@gmail.com>
2015-11-07 01:06:54 +0900

    Make site work.

--- src/site.hs
+++ src/site.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Default (def)
-import qualified Data.Map as Map
-import Data.Maybe (fromMaybe)
 import Hakyll
     ( Compiler, Configuration(..), Context, Identifier, Item
     , Routes, applyAsTemplate, compile, composeRoutes, compressCssCompiler
@@ -30,7 +28,7 @@ main = hakyllWith hakyllConfig $ do
     match "templates/*" $ compile templateCompiler
 
     -- images
-    match "img/*" $ do
+    match "images/*" $ do
         route idRoute
         compile copyFileCompiler
 
@@ -53,43 +51,9 @@ main = hakyllWith hakyllConfig $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            -- Instead of just (return posts) here, I might want to take
-            -- each post Item and add a field about whether or not it is
-            -- the last Item.  Then, in templates/post-list.html, I can
-            -- look at whether or not it is last, and only add a <hr> if it
-            -- is not last.  Also, it should be ignored if the "draft"
-            -- metadata is set, because it won't be shown.
-            --
-            -- The following post might help?
-            -- https://github.com/jaspervdj/hakyll/issues/263
-            let indexCtx = listField "posts" postCtx (return posts) `mappend`
-                           defaultContext
             indexBody <- getResourceBody
-            indexWithContext <- applyAsTemplate indexCtx indexBody
-            applyDefaultTemplate indexCtx indexWithContext
-
-    -- blog posts
-    match "posts/*" $ do
-        route postsAndDraftsRoutes
-        compile $ do
-            let subHeadingCtx =
-                    field "subHeadingContent" createSubHeadingContentForPost `mappend`
-                    postCtx
-            pandocOut <- pandocCompiler
-            postTemplateOut <- loadAndApplyTemplate postTemplate subHeadingCtx pandocOut
-            applyDefaultTemplate subHeadingCtx postTemplateOut
-
-    -- these are just example sites to get ideas from when designing
-    -- our site
-    match "bootstrap-templates/**" $ do
-        route idRoute
-        compile copyFileCompiler
-
--- | For posts, add a @date@ field to the default context.
-postCtx :: Context String
-postCtx = dateField "date" "%B %e, %Y" `mappend`
-          defaultContext
+            indexWithContext <- applyAsTemplate defaultContext indexBody
+            applyDefaultTemplate defaultContext indexWithContext
 
 -- | Apply the default template and then relativize all the URLs in the
 -- resulting html file.
@@ -102,34 +66,3 @@ applyDefaultTemplate context preTemplateItem = do
 -- through.  This default template defines our site HTML \<head\> tag.
 defaultTemplate :: Identifier
 defaultTemplate = "templates/default.html"
-
--- | This is the default template for posts.  It basically just defines an
--- \<article\> wrapper for the post body.
-postTemplate :: Identifier
-postTemplate = "templates/post.html"
-
--- | Create the HTML tags for the subheading and "Posted by" lines for
--- a blog post.
---
--- This is kind of hacky.
-createSubHeadingContentForPost :: Item a -> Compiler String
-createSubHeadingContentForPost item = do
-    let ident = itemIdentifier item
-    maybeSubHeading <- getMetadataField ident "subHeading"
-    maybePostedBy <- getMetadataField ident "postedBy"
-    let subHeading = fromMaybe "" maybeSubHeading
-        subHeadingHtml = "<h2 class=\"subheading\">" ++ subHeading ++ "</h2>"
-        postedBy = fromMaybe "" maybePostedBy
-        postedByHtml = "<span class=\"meta\">Posted by " ++ postedBy ++ "</span>"
-    return $ subHeadingHtml ++ postedByHtml
-
--- | If posts have a "draft" metadata, then this changes their output
--- location from "posts/" to "drafts/".
-postsAndDraftsRoutes :: Routes
-postsAndDraftsRoutes = metadataRoute $ \metadata ->
-    case Map.lookup "draft" metadata of
-        Just _ ->
-            gsubRoute "posts/" (const "drafts/") `composeRoutes`
-            setExtension "html"
-        Nothing -> setExtension "html"
-
